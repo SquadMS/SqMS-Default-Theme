7ae45f1854764ed9fc27398b6620feb9a0f9d69f
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-19 21:59:52 +0200

    Render translation as html

--- resources/views/structure/footer.blade.php
+++ resources/views/structure/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col text-center">
-                {{ __('squadms-default-theme::footer.powered-by') }}
+                {!! __('squadms-default-theme::footer.powered-by') !!}
             </div>
         </div>
     </div>
