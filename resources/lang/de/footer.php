187cbe1d93b900f1072a40d1845c0b721e1d91aa
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-19 22:00:49 +0200

    Change footer logo,

--- resources/lang/de/footer.php
+++ resources/lang/de/footer.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'powered-by' => 'Stolz angetrieben durch <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo-white.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>'
+    'powered-by' => 'Stolz angetrieben durch <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>'
 ];
