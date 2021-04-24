f03399e78aece8914820b3de2fb5913f93e3b248
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-25 01:42:04 +0200

    Fix home page logo responsiveness, Fix footer columns,

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col text-center">
                 <h1>Welcome to</h1>
-                <img src="https://squadms.com/img/logo.svg">
+                <img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid">
             </div>
         </div>
     </div>
