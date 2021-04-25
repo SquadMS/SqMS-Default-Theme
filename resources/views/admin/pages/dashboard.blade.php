314a6a9cdbd83ccfd3583e60ab6b079dce3ad472
Patrick Henninger <patrick.henninger@contagt.com>
2021-04-25 02:00:04 +0200

    Add basic Dashboard template content,

--- resources/views/admin/pages/dashboard.blade.php
+++ resources/views/admin/pages/dashboard.blade.php
@@ -1,5 +1,19 @@
 @extends('squadms-default-theme::admin.structure.layout')
 
 @section('content')
-
+<section>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <h1>Dashboard</h1>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-center">
+                <p class="color-info">Coming Soon.</p>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
