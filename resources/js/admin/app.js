714183a0d0cc0dc0c25d2861bd69da3590158f45
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-01 13:49:32 +0200

    Fix nav dropdown class, Change js build,

--- resources/js/admin/app.js
+++ resources/js/admin/app.js
@@ -4,4 +4,4 @@
 // If you do not need all plugins and want to improve build times only load what you need
 //import Alert from 'bootstrap/js/dist/alert';
 
-import Collapse from 'bootstrap/js/src/collapse';
+import { Collapse } from 'bootstrap';
--- resources/js/public/app.js
+++ resources/js/public/app.js
@@ -4,5 +4,4 @@
 // If you do not need all plugins and want to improve build times only load what you need
 //import Alert from 'bootstrap/js/src/alert';
 
-import Collapse from 'bootstrap/js/src/collapse';
-import Dropdown from 'bootstrap/js/src/dropdown';
+import { Collapse, Dropdown } from 'bootstrap';
