bd03309e8c822c4d79a5cdc50c807a2df7ae6923
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-01 14:00:06 +0200

    Work on navigatio dropdown,

--- resources/views/components/navigation/dropdown.blade.php
+++ resources/views/components/navigation/dropdown.blade.php
@@ -1,11 +1,11 @@
-@props(['active' => false, 'title'])
+@props(['id' => \Illuminate\Support\Str::random(40), 'active' => false, 'title'])
 
 <li {{ $attributes->merge(['class' => 'nav-item dropdown' . ($active ? ' active' : '')]) }}>
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+    <a class="nav-link dropdown-toggle" href="#" id="{{ $id }}" role="button"
         data-bs-toggle="dropdown" aria-expanded="false">
         {{ $title }}
     </a>
-    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="{{ $id }}">
         {{ $links }}
     </ul>
 </li>
