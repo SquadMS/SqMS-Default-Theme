87b166b74de6d4ba2690ec50e2e19add56428487
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-01 13:13:06 +0200

    Add hebrew translations,

---
+++ resources/lang/he/footer.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'copyright' => '© :year :name. כל הזכויות שמורות!',
+    'powered-by' => 'מופעל בגאווה על ידי <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>'
+];
---
+++ resources/lang/he/navigation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'home' => 'הביתה',
+    'profile' => 'פרופיל',
+
+    'admin' => 'Admin',
+    'login' => 'כניסה',
+    'logout' => 'יציאה',
+];
