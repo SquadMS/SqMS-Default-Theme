d0c53f9138ccddfca7a03ea529a71ecc03530b5c
ibnalsam <47418168+ibnalsam@users.noreply.github.com>
2021-05-07 05:08:21 -0700

    Update footer.php

--- resources/lang/ar/footer.php
+++ resources/lang/ar/footer.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'copyright' => '© :year :name. All rights reserved!',
-    'powered-by' => 'Proudly powered by <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>'
+    'copyright' => '© :year :name. جميع الحقوق محفوظة !',
+    'powered-by' => 'مدعوم بكل فخر من قبل <a href="https://squadms.com" target="_blank"><img src="https://squadms.com/img/logo.svg" alt="SquadMS Logo" class="img-fluid" style="height:1em"></a>'
 ];
