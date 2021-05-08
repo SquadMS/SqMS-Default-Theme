1b34c48134c44f5ab004409c34792f9536955736
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-08 18:43:38 +0200

    Added basic RBAC admin page template,

---
+++ resources/views/admin/pages/rbac.blade.php
@@ -0,0 +1,19 @@
+@extends('squadms-default-theme::admin.structure.layout')
+
+@section('content')
+<section>
+    <div class="container-fluid">
+        <div class="row pb-4">
+            <div class="col">
+                <h1>RBAC</h1>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col text-center">
+                <p class="color-info">Coming Soon.</p>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
