f9cbaf90a40314f0550189cd10ffb45248d655cd
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-09 14:50:57 +0200

    Remove uppercase from button default,

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn text-uppercase']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'btn']) }}>
     {{ $slot }}
 </button>
