1ce72105fd0bac047bbe5f1a1adff8b4768d64cf
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-09 16:38:33 +0200

    Fix

--- resources/views/admin/pages/rbac.blade.php
+++ resources/views/admin/pages/rbac.blade.php
@@ -7,14 +7,14 @@
             <div class="col">
                 <h1 class="d-flex aliign-items-center">
                     <span class="flex-grow-1">RBAC</span>
-                    <livewire:sqms-default-theme.admin.rbac.create-role></livewire:admin.rbac.create-role/>
+                    <livewire:sqms-default-theme.admin.rbac.create-role></livewire:sqms-default-theme.admin.rbac.create-role/>
                 </h1>
                 <hr>
             </div>
         </div>
         <div class="row">
             <div class="col text-center">
-                <livewire:sqms-default-theme.admin.rbac.role-list></livewire:admin.rbac.role-list/>
+                <livewire:sqms-default-theme.admin.rbac.role-list></livewire:sqms-default-theme.admin.rbac.role-list/>
             </div>
         </div>
     </div>
