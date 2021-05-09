72555ab16d8d6b75ba26c79d18312da368c7ca18
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-09 16:40:59 +0200

    Fix

--- src/Providers/LivewireServiceProvider.php
+++ src/Providers/LivewireServiceProvider.php
@@ -5,6 +5,9 @@ namespace SquadMS\DefaultTheme\Providers;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 use SquadMS\DefaultTheme\Http\Livewire\RBAC\CreateRole;
+use SquadMS\DefaultTheme\Http\Livewire\RBAC\DeleteRole;
+use SquadMS\DefaultTheme\Http\Livewire\RBAC\EditRole;
+use SquadMS\DefaultTheme\Http\Livewire\RBAC\MembersRole;
 use SquadMS\DefaultTheme\Http\Livewire\RBAC\RoleList;
 
 class LivewireServiceProvider extends ServiceProvider
@@ -29,5 +32,8 @@ class LivewireServiceProvider extends ServiceProvider
         /* Register livewire components */
         Livewire::component('sqms-default-theme.admin.rbac.role-list', RoleList::class);
         Livewire::component('sqms-default-theme.admin.rbac.create-role', CreateRole::class);
+        Livewire::component('sqms-default-theme.admin.rbac.create-role', EditRole::class);
+        Livewire::component('sqms-default-theme.admin.rbac.create-role', DeleteRole::class);
+        Livewire::component('sqms-default-theme.admin.rbac.create-role', MembersRole::class);
     }
 }
