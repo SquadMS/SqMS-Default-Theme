4a0f281a1e1667f7ce785d00517e39e527b50b97
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-09 13:30:22 +0200

    Fix

--- src/Http/Livewire/RBAC/CreateRole.php
+++ src/Http/Livewire/RBAC/CreateRole.php
@@ -22,6 +22,6 @@ class CreateRole extends Component
     
     public function render()
     {
-        return view('squadms-default-theme::admin.livewire.create-role');
+        return view('squadms-default-theme::admin.livewire.rbac.create-role');
     }
 }
