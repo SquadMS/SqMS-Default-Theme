19fbd7115396df906f33c40742fe8e34e5f099e1
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-09 13:18:34 +0200

    Fix,

--- src/Http/Livewire/RBAC/RoleList.php
+++ src/Http/Livewire/RBAC/RoleList.php
@@ -15,7 +15,7 @@ class RoleList extends Component
     public function render()
     {
         return view('admin.livewire.role-list', [
-            'roles' => Role::all()->paginate(10),
+            'roles' => Role::paginate(10),
         ]);
     }
 }
