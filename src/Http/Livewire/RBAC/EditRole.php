2809494f5a0afacd780e8d017975975c87a0c7f5
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-12 19:52:15 +0200

    Fix class file names,

--- src/Http/Livewire/RBAC/EditRole.php
+++ src/Http/Livewire/RBAC/EditRole.php
@@ -5,13 +5,31 @@ namespace SquadMS\DefaultTheme\Http\Livewire\RBAC;
 use Livewire\Component;
 use Spatie\Permission\Models\Role;
 
-class MembersRole extends Component
+class EditRole extends Component
 {
     public bool $showModal = false;
     public Role $role;
+
+    public function updateRole() {
+        /* Validate the data first */
+        $this->validate($this->getValidationRules());
+        
+        /* Create the Role */
+        $this->role->save();
+
+        /* Emit event */
+        $this->emit('role:updated');
+    }
     
     public function render()
     {
-        return view('squadms-default-theme::admin.livewire.rbac.members-role');
+        return view('squadms-default-theme::admin.livewire.rbac.edit-role');
+    }
+
+    private function getValidationRules() : array
+    {
+        return [
+            'role.name' => 'required|string|unique:Spatie\Permission\Models\Role,name,' . $this->role->id,
+        ];
     }
 }
