5904ffe18e3c028691f922a5908c550a35c231a4
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-15 04:56:01 +0200

    Add selectedOption method, Further fix styles,

--- src/Http/Livewire/Contracts/LivewireSelect.php
+++ src/Http/Livewire/Contracts/LivewireSelect.php
@@ -3,6 +3,7 @@
 namespace SquadMS\DefaultTheme\Http\Livewire\Contracts;
 
 use Asantibanez\LivewireSelect\LivewireSelect as Component;
+use SquadMS\Foundation\Repositories\UserRepository;
 
 /**
  * @inheritDoc
@@ -20,11 +21,11 @@ abstract class LivewireSelect extends Component
 
             'search' => 'position-relative',
             'searchInput' => 'form-control',
-            'searchOptionsContainer' => 'position-absolute top-0 left-0 mt-5 w-100 zindex-dropdown border border-grey rounded',
+            'searchOptionsContainer' => 'position-absolute top-0 left-0 mt-5 w-100 zindex-dropdown bg-light border border-grey rounded',
 
-            'searchOptionItem' => 'p-3 cursor-pointer bg-light',
-            'searchOptionItemActive' => 'p-3 cursor-pointer bg-primary text-white',
-            'searchOptionItemInactive' => 'bg-gray',
+            'searchOptionItem' => 'p-3 cursor-pointer',
+            'searchOptionItemActive' => 'bg-primary text-white',
+            'searchOptionItemInactive' => '',
 
             'searchNoResults' => 'p-3 w-100 bg-white border text-center',
         ];
--- src/Http/Livewire/RBAC/NewMemberSearch.php
+++ src/Http/Livewire/RBAC/NewMemberSearch.php
@@ -10,12 +10,21 @@ class NewMemberSearch extends LivewireSelect
 {
     public function options($searchTerm = null) : Collection 
     {
-        return UserRepository::getUserModelQuery()->where('name', 'like', $searchTerm)
+        return UserRepository::getUserModelQuery()
+        ->where('name', 'like', $searchTerm)
         ->orWhere('name', 'like', '%' . $searchTerm)
         ->orWhere('name', 'like', $searchTerm . '%')
         ->orWhere('name', 'like', '%'. $searchTerm . '%')
         ->limit(10)
         ->get()
         ->map(fn ($user) => [ 'value' => $user['steam_id_64'], 'description' => $user['name'] ]);
+    }
+
+    public function selectedOption($steamId64)
+    {
+        $user = UserRepository::getUserModelQuery()
+        ->where('steam_id_64', $steamId64)
+        ->map(fn ($user) => [ 'value' => $user['steam_id_64'], 'description' => $user['name'] ])
+        ->toArray();
     }
 }
