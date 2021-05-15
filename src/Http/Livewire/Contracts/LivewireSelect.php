d6c421a81bab6d291cfa2767bd451c24efc3e6d3
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-15 04:44:00 +0200

    Fix styles

--- src/Http/Livewire/Contracts/LivewireSelect.php
+++ src/Http/Livewire/Contracts/LivewireSelect.php
@@ -20,11 +20,11 @@ abstract class LivewireSelect extends Component
 
             'search' => 'position-relative',
             'searchInput' => 'form-control',
-            'searchOptionsContainer' => 'position-absolute top-0 left-0 mt-5 w-100 zindex-dropdown',
+            'searchOptionsContainer' => 'position-absolute top-0 left-0 mt-5 w-100 zindex-dropdown border border-grey rounded',
 
             'searchOptionItem' => 'p-3 cursor-pointer text-sm',
-            'searchOptionItemActive' => 'p-3 cursor-pointer bg-gray',
-            'searchOptionItemInactive' => 'bg-gray-dark',
+            'searchOptionItemActive' => 'p-3 cursor-pointer bg-light',
+            'searchOptionItemInactive' => 'bg-gray',
 
             'searchNoResults' => 'p-3 w-100 bg-white border text-center',
         ];
