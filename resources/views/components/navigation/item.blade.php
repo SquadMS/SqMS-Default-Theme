cdebcf33f9f4d1acbd23ce1b9e4e6e87a740651d
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-17 01:08:16 +0200

    Fix,

--- resources/views/components/navigation/item.blade.php
+++ resources/views/components/navigation/item.blade.php
@@ -1,5 +1,5 @@
 @props(['active' => false, 'disabled' => false, 'link' => '#', 'onClick' => false, 'title'])
 
 <li {{ isset($attributes) ? $attributes->merge(['class' => 'nav-item']) : '' }}>
-    <a class="nav-link {{ $disabled ? ' disabled' : '' }} {{ $active ? ' active' : '' }}" href="{{ $link }}" {{ $onClick ? 'onClick="' . $onClick . '"' : ''}}>{!! $title !!}</a>
+    <a class="nav-link {{ $disabled ? ' disabled' : '' }} {{ $active ? ' active' : '' }}" href="{{ is_callable($link) ? $link() : $link }}" {{ $onClick ? 'onClick="' . $onClick . '"' : ''}}>{!! $title !!}</a>
 </li>
