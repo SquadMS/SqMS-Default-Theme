0b5967d47d89430b1f1285fbeb347ea1608f94fa
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-29 04:53:56 +0200

    Add servers page template and translations,

---
+++ resources/views/pages/servers.blade.php
@@ -0,0 +1,13 @@
+@extends('sqms-default-theme::structure.layout')
+
+@section('content')
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>{{ __('sqms-default-theme::pages/servers.heading') }}</h1>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
