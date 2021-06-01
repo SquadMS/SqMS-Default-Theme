01d76eb4ffb930962af6797269a407156224e500
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-01 06:27:30 +0200

    Use light instead of primary, More touches to styling,

--- resources/views/pages/servers.blade.php
+++ resources/views/pages/servers.blade.php
@@ -11,19 +11,21 @@
         <div class="row server-list">
             @foreach ($servers as $server)
                 <div class="col-12 mb-4">
-                    <div class="server d-flex flex-column flex-md-row bg-light bg-no-map bg-cover bg-center">
-                        <div class="d-flex align-items-center flex-md-grow-1 p-4 main-info">
-                            <span class="w-100 h2 text-truncate text-center text-md-start text-white">{{ $server->name }}</span>
-                        </div>
+                    <div class="server bg-light bg-no-map bg-cover bg-center">
+                        <div class="server-inner d-flex flex-column flex-md-row">
+                            <div class="main-info d-flex align-items-center flex-md-grow-1 p-4">
+                                <span class="w-100 h2 text-truncate text-center text-md-start text-white">{{ $server->name }}</span>
+                            </div>
+        
+                            <div class="actions d-flex flex-row align-items-stretch">
+                                <a href="{{ $server->connect_url }}" class="d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center bg-transparent-steam-300 px-3 py-2 px-md-5 py-md-4 text-decoration-none">
+                                    <span class="text-truncate"><i class="bi bi-controller"></i><span class="d-none d-md-inline"> Join</span></span>
+                                </a>
     
-                        <div class="d-flex flex-row align-items-stretch">
-                            <a href="{{ $server->connect_url }}" class="d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center bg-transparent-steam-300 px-3 py-2 px-md-5 py-md-4">
-                                <span class="text-truncate"><i class="bi bi-controller"></i> Join</span>
-                            </a>
-
-                            <a href="#" class="d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center bg-transparent-info-300 px-3 py-2 px-md-5 py-md-4">
-                                <span class="text-truncate"><i class="bi bi-info-circle-fill"></i> Details</span>
-                            </a>
+                                <a href="#" class="d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center bg-transparent-info-300 px-3 py-2 px-md-5 py-md-4 text-decoration-none">
+                                    <span class="text-truncate"><i class="bi bi-info-circle-fill"></i><span class="d-none d-md-inline"> Details</span></span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
