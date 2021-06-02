a689c684c218e0757075e5f88ced6a1d8f84f232
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-02 22:22:26 +0200

    Update module definition,

--- src/SquadMSModule.php
+++ src/SquadMSModule.php
@@ -2,6 +2,7 @@
 
 namespace SquadMS\DefaultTheme;
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Support\Facades\Artisan;
 use SquadMS\Foundation\Modularity\Contracts\SquadMSModule as SquadMSModuleContract;
 
@@ -31,6 +32,11 @@ class SquadMSModule extends SquadMSModuleContract {
     }
 
     static function registerMenuEntries(string $menu) : void
+    {
+        //
+    }
+
+    static function schedule(Schedule $schedule) : void
     {
         //
     }
