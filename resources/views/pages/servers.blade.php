1336f3d6a1c094d14b5e4099a4b0744f07363936
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-03 10:50:48 +0200

    Change coloring of server actions and info,

--- resources/views/pages/servers.blade.php
+++ resources/views/pages/servers.blade.php
@@ -18,22 +18,23 @@
                         <div class="server-inner d-flex flex-column flex-md-row">
                             <div class="main-info d-flex flex-column flex-md-row align-items-md-center flex-md-grow-1 min-width-0">
                                 <div class="flex-md-grow-1 min-width-0 d-flex align-items-center p-4">
-                                    <span class="w-100 h2 text-truncate text-center text-md-start text-white mb-md-0 data-server-name data-show-online {{ \Illuminate\Support\Arr::get($status, 'online', false) ? '' : 'd-none' }}">{{ \Illuminate\Support\Arr::get($status, 'name', 'Squad Dedicated Server') }}</span>
-                                    <span class="w-100 h2 text-truncate text-center text-md-start text-secondary mb-md-0 data-show-offline {{ \Illuminate\Support\Arr::get($status, 'online', false) ? 'd-none' : '' }}">{{ $server->name }}</span>
+                                    <span class="w-100 h3 text-truncate text-center text-md-start text-white mb-md-0 data-server-name data-show-online {{ \Illuminate\Support\Arr::get($status, 'online', false) ? '' : 'd-none' }}">{{ \Illuminate\Support\Arr::get($status, 'name', 'Squad Dedicated Server') }}</span>
+                                    <span class="w-100 h3 text-truncate text-center text-md-start text-secondary mb-md-0 data-show-offline {{ \Illuminate\Support\Arr::get($status, 'online', false) ? 'd-none' : '' }}">{{ $server->name }}</span>
                                 </div>
-                                <div class="flex-grow-1 flex-md-grow-0 d-flex h-100 align-items-center justify-content-center p-2 px-md-4 info">
+                                
+                                <div class="flex-grow-1 flex-md-grow-0 d-flex h-100 align-items-center justify-content-center p-2 px-md-4 extra">
                                     <span class="text-white data-show-online {{ \Illuminate\Support\Arr::get($status, 'online', false) ? '' : 'd-none' }}"><span class="data-count">{{ \Illuminate\Support\Arr::get($status, 'playerCount', 0) }}</span>(+<span class="data-queue">{{ intval(\Illuminate\Support\Arr::get($status, 'queue', '0')) }}</span>)/<span class="data-slots">{{ intval(\Illuminate\Support\Arr::get($status, 'slots', '0')) }}</span>(+<span class="data-reserved">{{ \Illuminate\Support\Arr::get($status, 'reservedSlots', '0') }}</span>) {{ __('sqms-default-theme::pages/servers.server.players') }}</span>
                                     <span class="text-danger data-show-offline {{ \Illuminate\Support\Arr::get($status, 'online', false) ? 'd-none' : '' }}">{{ __('sqms-default-theme::pages/servers.server.offline') }}</span>
                                 </div>
                             </div>
         
                             <div class="actions d-flex flex-row align-items-stretch">
-                                <a href="{{ $server->connect_url }}" class="d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center bg-transparent-steam-800 px-3 py-2 px-md-4 py-md-1 text-decoration-none">
-                                    <span class="text-truncate lh-1"><i class="bi bi-controller"></i></span>
+                                <a href="{{ $server->connect_url }}" class="extra d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center px-3 py-2 px-md-4 py-md-1 text-decoration-none">
+                                    <span class="text-steam lh-1"><i class="bi bi-controller"></i></span>
                                 </a>
     
-                                <a href="#" class="d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center bg-transparent-info-800 px-3 py-2 px-md-4 py-md-1 text-decoration-none">
-                                    <span class="text-truncate lh-1"><i class="bi bi-info-circle-fill"></i></span>
+                                <a href="#" class="extra d-flex flex-grow-1 flex-md-grow-0 align-items-center justify-content-center px-3 py-2 px-md-4 py-md-1 text-decoration-none">
+                                    <span class="text-primary lh-1"><i class="bi bi-info-circle-fill"></i></span>
                                 </a>
                             </div>
                         </div>
