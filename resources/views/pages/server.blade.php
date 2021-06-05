2de138bf123e6e666678542d9cb550b45b4e2434
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-05 16:51:38 +0200

    Remove unecessary variable, Correctly use BS5 ratio on server view,

--- resources/views/pages/server.blade.php
+++ resources/views/pages/server.blade.php
@@ -7,8 +7,8 @@
             <div class="col">
                 @if ($server->last_query_result->online())
                     @foreach ($server->last_query_result->population()->getTeams() as $team)
-                    <div class="embed-responsive embed-responsive-squad-flag bg-faction-{{ \SquadMS\Foundation\Helpers\FactionHelper::getFactionTag($team->getName(), $server->last_query_result->layer()) }} bg-cover bg-center">
-                        <div class="embed-responsive-item d-flex justify-content-center align-items-center">
+                    <div class="ratio ratio-squad-flag bg-faction-{{ \SquadMS\Foundation\Helpers\FactionHelper::getFactionTag($team->getName(), $server->last_query_result->layer()) }} bg-cover bg-center">
+                        <div class="d-flex justify-content-center align-items-center">
                             <div class="gradient position-absolute w-100 h-100"></div>
                             <h2 class="text-white text-nowrap text-truncate w-100 px-2 mb-0" style="z-index: 1">{{ $team->getName() }}</iframe>
                         </div>
