39078fe511cdad4b21cead531a559b7cacc9e153
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-08 18:27:49 +0200

    Only register module routes if the module is actually installed,

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Config;
 use SquadMS\Foundation\Facades\SquadMSRouter;
 
-/* Define routes from config */
-SquadMSRouter::webRoutes(Config::get('sqms-default-theme.routes.def', []));
+/* Get route definitions from config */
+$definitions = Config::get('sqms-default-theme.routes.def', []);
+
+$modules = [
+    'SquadMS\\Servers\\SquadMSModule' => ['server-population'],
+];
+
+/* Register all definitions except those for modules */
+SquadMSRouter::webRoutes(Arr::except($definitions, Arr::flatten($modules)));
+
+foreach ($modules as $module => $definitions) {
+    /* Check if the SquadMSModule class does exist */
+    if (class_exists($module)) {
+        /* Register definitions for this module */
+        SquadMSRouter::webRoutes(Arr::only($definitions, $definitions));
+    }
+}
