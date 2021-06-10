cde31e5f59958b038c81153bc46e878501e83ec0
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-10 20:31:44 +0200

    Fix

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,16 @@ use SquadMS\Foundation\Facades\SquadMSRouter;
 $definitions = Config::get('sqms-default-theme.routes.def', []);
 
 $modules = [
-    '\\SquadMS\\Servers\\SquadMSModule' => ['server-population'],
+    'SquadMS\\Servers\\SquadMSModule' => ['server-population'],
 ];
 
 /* Register all definitions except those for modules */
 SquadMSRouter::webRoutes(Arr::except($definitions, Arr::flatten($modules)));
 
-foreach ($modules as $module => $definitions) {
+foreach ($modules as $module => $definitionNames) {
     /* Check if the SquadMSModule class does exist */
     if (class_exists($module)) {
         /* Register definitions for this module */
-        SquadMSRouter::webRoutes(Arr::only($definitions, $definitions));
+        SquadMSRouter::webRoutes(Arr::only($definitions, $definitionNames));
     }
 }
