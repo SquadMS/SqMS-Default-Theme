1e687d61ebd0a0b6ee689014cca122f6f8713dc7
Patrick <patrick.henninger@contagt.com>
2022-06-13 20:14:16 +0200

    Update sqms-default-theme.php

--- config/sqms-default-theme.php
+++ config/sqms-default-theme.php
@@ -1,19 +1,5 @@
 <?php
 
 return [
-    'routes' => [
-        'prefix' => null,
-        'middleware' => ['web'],
-        'def' => [
-            'server-population' => [
-                'type' => 'get',
-                'name' => 'server',
-                'path' => 'servers/{server}/population',
-                'middlewares' => [],
-                'controller' => \SquadMS\DefaultTheme\Http\Controllers\ServerController::class,
-                'executor' => 'population',
-                'localized' => true,
-            ],
-        ]
-    ],
+    //
 ];
