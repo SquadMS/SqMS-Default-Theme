236ab9376f65bc2d9a5903db7e64c9a0354774b6
Patrick <patrick.henninger@contagt.com>
2022-06-13 20:12:47 +0200

    Delete resources/views/components/player-list directory

--- resources/views/components/player-list/player.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="player d-flex min-width-0 p-2 border-start border-end border-bottom border-light" player-id="{{ $player->getSteamId() }}">
-    <a href="{{ route('profile', $player->getSteamId()) }}" class="text-truncate text-decoration-none">{{ $player->getName() }}</a>
-</div>
